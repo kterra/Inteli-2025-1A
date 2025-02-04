7538f04c40642a36d7ded486ee6b6a7dcfd36f99
Kizzy Terra <kizzyterra@gmail.com>
2025-02-04 15:07:41 -0300

    Add files via upload

---
+++ semana01/atividade1.js
@@ -0,0 +1,14 @@
+// 📚 Programa para armazenar e exibir informações de um aluno
+
+// Definição de variáveis
+var nome = "Kizzy Terra";
+var idade = 32;
+var escola = "CMPA";
+var notaFinal = 9.5;
+
+// Exibição das informações no console
+console.log("*Dados do Aluno*");
+console.log("Nome:", nome);
+console.log("Idade:", idade, "anos");
+console.log("Escola:", escola);
+console.log("Nota Final:", notaFinal);
---
+++ semana01/variaveis.js
@@ -0,0 +1,44 @@
+//Variáveis na prática
+
+let gaveta_1 = 3;
+let gaveta_9 = 3 + 9;
+
+let armario = gaveta_1 + gaveta_9;
+console.log(armario);
+
+gaveta_9 = 10;
+console.log(gaveta_9);
+
+armario = gaveta_1 + gaveta_9;
+console.log(armario);
+
+console.log('Olá mundo!');
+
+
+// var
+var x = 10;
+var x = 20;  // ✅ Permitido
+console.log(x); // 20
+
+// let
+let y = 30;
+y = 40;  // ✅ Permitido
+// let y = 50;  // ❌ Erro: já foi declarado
+
+// const
+const z = 50;
+// z = 60;  // ❌ Erro: não pode ser reatribuído
+// const z = 70;  // ❌ Erro: não pode ser redeclarado
+
+if (true) {
+    w = 10;  // Declarado com var (escopo de função)
+    let q = 50;  // Declarado com let (escopo de bloco)
+    console.log(z); // ✅ Funciona
+
+}
+
+console.log(w); // ✅ Funciona
+var w = 0;
+console.log(w); // ✅ Funciona
+
+console.log(q); // ❌ Erro! let tem escopo de bloco e não pode ser acessado aqui
